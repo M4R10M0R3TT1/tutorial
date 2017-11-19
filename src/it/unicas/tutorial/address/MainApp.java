8de4ede702c6713d146a8dba1973ecfe606473f5
M4R10M0R3TT1 <0035316@studentmail.unicas.it>
2017-11-19 13:12:29 +0100

    Aggiunto DarkTheme.css, ridimensionate le varie sezioni grafiche, aggiunte icone per RootLayout.fxml e PersonEditDialog.fxml

--- src/it/unicas/tutorial/address/MainApp.java
+++ src/it/unicas/tutorial/address/MainApp.java
@@ -11,6 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -25,6 +26,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/agenda-128.png"));
 
         initRootLayout();
 
@@ -106,6 +110,9 @@ public class MainApp extends Application {
             PersonEditDialogController controller = loader.getController();
             controller.setDialogStage(dialogStage);
             controller.setPerson(person);
+
+            // Set the application icon.
+            dialogStage.getIcons().add(new Image("file:resources/images/pencil-lapis-128.png"));
 
             // Show the dialog and wait until the user closes it
             dialogStage.showAndWait();
